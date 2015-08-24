7d77f5fc6c75c8e359ba6137ee2af895f0ed1c73
Marcus Haase <HaaseIT@users.noreply.github.com>
2015-08-24 12:36:30 +0200

    added config loading

--- mailer.php
+++ mailer.php
@@ -13,31 +13,11 @@ date_default_timezone_set("Europe/Berlin");
 include_once('vendor/autoload.php');
 //include_once('functions.misc.inc.php');
 
-define("ATTACHMENT_DIRECTORY", 'attachments/');
-define("ATTACHMENT_DIRECTORY_EMBEDDEDIMG", 'img/');
-
-$C = array(
-    'mail_to' => array(
-        /*
-         either one email address per row or an array containing one email address per row
-         a not valid email address will be used as a headline (disabled option)
-        eg:
-        'mail@john.doe',
-        array(
-            'mail@john.doe',
-            'mail@jane.doe',
-        ),
-         */
-        'mail@john.doe',
-    ),
-    'mail_from_user' => 'Marcus Haase',
-    'mail_from_email' => 'mail@marcus.haase.name',
-    'mail_subject' => 'HTML Testmail - '.date("Y-m-d H:i:s"),
-    'log_mails' => true,
-    'premailer_enable' => true,
-    'premailer_executable' => 'premailer',
-    'premailer_generate_plaintext' => false,
-);
+if (is_file(./config.php)) {
+    require './config.php';
+} else {
+    require './config.dist.php';
+}
 
 // Check for available attachment images for embedding
 $hDirEmbImgAttachments = opendir('./'.ATTACHMENT_DIRECTORY.ATTACHMENT_DIRECTORY_EMBEDDEDIMG);
