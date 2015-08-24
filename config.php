dad2c6b5dacdc9386514c2037fc55607fb83340d
Marcus Haase <HaaseIT@users.noreply.github.com>
2015-08-24 12:34:38 +0200

    initial import

---
+++ config.php
@@ -0,0 +1,25 @@
+define("ATTACHMENT_DIRECTORY", 'attachments/');
+define("ATTACHMENT_DIRECTORY_EMBEDDEDIMG", 'img/');
+
+$C = array(
+    'mail_to' => array(
+        /*
+         either one email address per row or an array containing one email address per row
+         a not valid email address will be used as a headline (disabled option)
+        eg:
+        'mail@john.doe',
+        array(
+            'mail@john.doe',
+            'mail@jane.doe',
+        ),
+         */
+        'mail@john.doe',
+    ),
+    'mail_from_user' => 'Marcus Haase',
+    'mail_from_email' => 'mail@marcus.haase.name',
+    'mail_subject' => 'HTML Testmail - '.date("Y-m-d H:i:s"),
+    'log_mails' => true,
+    'premailer_enable' => true,
+    'premailer_executable' => 'premailer',
+    'premailer_generate_plaintext' => false,
+);
